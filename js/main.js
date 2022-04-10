e38b268cf0612becd620385c93f9e6eac346ae69
LucaBonaldoIT <lucabonaldoit@gmail.com>
2022-04-10 20:18:35 +0200

    Basic site implementation
    Basic site implementation - added index.html with support to bootstrap, jquery and other libraries. The style for the website is under construction.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+
+
+    } else {
+        // jQuery is not loaded
+        document.write("Error loading JQuery");
+    }
+}
